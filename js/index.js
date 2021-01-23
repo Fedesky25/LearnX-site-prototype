554bb865525df3f32d89d8bc9308aa184ee5a1a7
Fedesky25 <fedesky.guglielmi@gmail.com>
2021-01-23 15:54:46 +0100

    bg & brand change + animations

--- js/index.js
+++ js/index.js
@@ -1,34 +1,32 @@
 const nav = document.getElementById('nav');
 const header = document.querySelector('header')
-document.querySelectorAll('.fader').forEach(element=>{
-    fade(element);
-    const duration = element.getAttribute('data-duration');
-    element.style.transition = `opacity ${duration}ms ease-out, transform ${duration}ms ease-out`;
-})
-function appear(target){
-    target.style.opacity = 1;
-    target.style.transform = 'translate(0)';
-}
-function fade(target){
-    const translation = target.getAttribute('data-translation');
-    target.style.opacity = 0;
-    target.style.transform = (translation) ? `translate(${translation})` : 'translateX(-100%)';
-}
-
+// document.querySelectorAll('.fader').forEach(element=>{
+//     fade(element);
+//     const duration = element.getAttribute('data-duration');
+//     element.style.transition = `opacity ${duration}ms ease-out, transform ${duration}ms ease-out`;
+// })
+// function appear(target){
+//     target.style.opacity = 1;
+//     target.style.transform = 'translate(0)';
+// }
+// function fade(target){
+//     const translation = target.getAttribute('data-translation');
+//     target.style.opacity = 0;
+//     target.style.transform = (translation) ? `translate(${translation})` : 'translateX(-100%)';
+// }
 const headerObs = new IntersectionObserver(entries=>{entries.forEach(entry=>{
-    if(entry.isIntersecting) fade(nav); else appear(nav);
-})}, {rootMargin: '-30px 0px 0px 0px'});
-
-window.addEventListener('resize', e=>{
-    if(window.innerWidth > 900) {nav.classList.add('fader'); headerObs.observe(header);}
-    else {nav.classList.remove('fader'); headerObs.unobserve(header);}
-})
-
+    if(entry.isIntersecting) nav.classList.remove('show'); else nav.classList.add('show');
+})}, {rootMargin: '-60px 0px 0px 0px'});
+function refreshNav() {
+    if(window.innerWidth > 900) {nav.classList.add('show'); headerObs.observe(header);}
+    else {nav.classList.remove('show'); headerObs.unobserve(header);}
+}
+refreshNav();
+window.addEventListener('resize', e=>{refreshNav()})
 document.querySelectorAll('nav a[href^="#"]').forEach(element=>{
     const target = document.getElementById(element.getAttribute('href').slice(1));
     element.addEventListener('click', e=>{e.preventDefault(); smoothScrollTo(target, 30, 600); target.blur();});
 })
-
 const water = document.querySelector('.water');
 let ticking = false;
 document.addEventListener('scroll', ()=>{
@@ -40,7 +38,6 @@ document.addEventListener('scroll', ()=>{
         ticking = true;
     }
 })
-
 const navToggler = document.getElementById('nav-toggler');
 const contents = document.querySelectorAll('header, section, footer');
 let navOpen = false;
